fe7e05f20bbc88e5b61c87d608c4ac8017fa49a5
unknown <imperialjapan869@gmail.com>
2025-03-25 15:09:05 +0530

    Update app.js with simplified configuration

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import courseRoutes from './routes/courseRoutes.js';
+import { config } from 'dotenv';
+
+config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
-
+// Middlewares
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/v1/course', courseRoutes);
+// Simple root route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to LMS API' });
+});
+
+// Import routes
+import userRouter from './routes/user.route.js';
+import courseRouter from './routes/course.route.js';
+import purchaseCourseRouter from './routes/purchaseCourse.route.js';
+import courseProgressRouter from './routes/courseProgress.route.js';
+
+// Use routes
+app.use('/api/v1', userRouter);
+app.use('/api/v1', courseRouter);
+app.use('/api/v1', purchaseCourseRouter);
+app.use('/api/v1', courseProgressRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
     success: false,
-    message: 'Something went wrong!'
+    message: err.message || 'Something went wrong!'
   });
 });
 
